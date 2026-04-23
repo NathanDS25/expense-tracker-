0ef0b0d3c1e4d2b3b4eca1bd3365a153dcb359b5
NathanDS25 <dsouzanathan25@gmail.com>
2026-04-24 01:56:48 +0530

    fix: remove generic SPA rewrite from vercel.json

--- server/test_db.cjs
+++ server/test_db.cjs
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://nathanneildsouza2021_db_user:gOKUkYEqNEiC2Nqs@expesne.cnfepvo.mongodb.net/expense_tracker?retryWrites=true&w=majority';
+const uri = 'mongodb://nathanneildsouza2021_db_user:gOKUkYEqNEiC2Nqs@ac-qxx6ph3-shard-00-00.cnfepvo.mongodb.net:27017,ac-qxx6ph3-shard-00-01.cnfepvo.mongodb.net:27017,ac-qxx6ph3-shard-00-02.cnfepvo.mongodb.net:27017/expense_tracker?ssl=true&replicaSet=atlas-qxx6ph3-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-mongoose.connect(uri)
+mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 })
   .then(() => {
     console.log('SUCCESS CONNECT');
     process.exit(0);
